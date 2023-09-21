1e42ecdace905ffde65458635883f05803d3aa9b
Piotr Storek <piotr.storek@dornach.eu>
2023-09-21 13:04:15 +0200

    VIP3

--- new_main.py
+++ new_main.py
@@ -11,17 +11,6 @@ import csv
 
 
 
-sensor.set_humidity_oversample(bme680.OS_2X)
-sensor.set_pressure_oversample(bme680.OS_4X)
-sensor.set_temperature_oversample(bme680.OS_8X)
-sensor.set_filter(bme680.FILTER_SIZE_3)
-sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
-
-sensor.set_gas_heater_temperature(320)
-sensor.set_gas_heater_duration(150)
-sensor.select_gas_heater_profile(0)
-
-
 start_time = time.time()
 curr_time = time.time()
 burn_in_time = 10
@@ -29,6 +18,14 @@ burn_in_data = []
 
 try:
     sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+    sensor.set_humidity_oversample(bme680.OS_2X)
+    sensor.set_pressure_oversample(bme680.OS_4X)
+    sensor.set_temperature_oversample(bme680.OS_8X)
+    sensor.set_filter(bme680.FILTER_SIZE_3)
+    sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
+    sensor.set_gas_heater_temperature(320)
+    sensor.set_gas_heater_duration(150)
+    sensor.select_gas_heater_profile(0)
 except IOError:
     sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
 
