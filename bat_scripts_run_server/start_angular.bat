c212e9564231742517880457577812d9f4390661
Piotr Storek <piotr.storek@dornach.eu>
2023-09-21 12:34:22 +0200

    Deleted app

--- bat_scripts_run_server/start_angular.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF 
-:: Migration latest data from GCP database
-TITLE Start angular app
-CD "C:\Users\Admin\PycharmProjects\DronSmogowy\NASA-Space-App-main"
-ng serve
--- bat_scripts_run_server/start_apache.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF 
-:: Migration latest data from GCP database
-TITLE Apache Server
-CD "C:\xampp"
-start call apache_start.bat
